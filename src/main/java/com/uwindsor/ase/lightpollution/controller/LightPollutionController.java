c6ccaa7d264dc14e0ed993962d1ec11c135942a3
SakshiWadhwa <SakshiWadhwa@users.noreply.github.com>
2022-07-20 21:56:56 -0400

    Adding Reported Count and Recommended Places API

--- src/main/java/com/uwindsor/ase/lightpollution/controller/LightPollutionController.java
+++ src/main/java/com/uwindsor/ase/lightpollution/controller/LightPollutionController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uwindsor.ase.lightpollution.dto.MessageResponse;
+import com.uwindsor.ase.lightpollution.dto.PeopleReportedCount;
 import com.uwindsor.ase.lightpollution.dto.PollutionReportList;
+import com.uwindsor.ase.lightpollution.dto.RecommendedPlaces;
 import com.uwindsor.ase.lightpollution.dto.SaveReportDto;
 import com.uwindsor.ase.lightpollution.service.LightPollutionService;
 
@@ -22,15 +25,30 @@ public class LightPollutionController {
 	public static final Logger logger = LogManager.getLogger(LightPollutionController.class);
 	
 	@Autowired
-	LightPollutionService lightPollutionervice;
+	LightPollutionService lightPollutionService;
 	
 	@PostMapping("lightPollution/report")
 	public ResponseEntity<MessageResponse> saveReport(@RequestBody SaveReportDto saveReportDto){
-		return lightPollutionervice.saveReport(saveReportDto);
+		return lightPollutionService.saveReport(saveReportDto);
 	}
 	
 	@GetMapping("lightPollution/report")
 	public PollutionReportList fetchReport(){
-		return lightPollutionervice.fetchReport();
+		return lightPollutionService.fetchReport();
+	}
+	
+	@GetMapping("lightPollution/reported_incidents/location/{location}")
+	public PeopleReportedCount fetchNumberOfPeopleReported(@PathVariable String location){
+		return lightPollutionService.fetchNumberOfPeopleReported(location);
+	}
+	
+	@GetMapping("lightPollution/recommend_place/province/{province}")
+	public RecommendedPlaces fetchRecommendPlacesBasedOnProvince(@PathVariable String province){
+		return lightPollutionService.fetchRecommendPlacesBasedOnProvince(province);
+	}
+	
+	@GetMapping("lightPollution/recommend_place/city/{city}")
+	public RecommendedPlaces fetchRecommendPlacesBasedOnCity(@PathVariable String city){
+		return lightPollutionService.fetchRecommendPlacesBasedOnCity(city);
 	}
 }
