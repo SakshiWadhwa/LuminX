2891ae4b6af13c314d89323ce2904a43a95eedad
SakshiWadhwa <SakshiWadhwa@users.noreply.github.com>
2022-07-20 22:09:26 -0400

    Update LightPollutionRepository.java

--- src/main/java/com/uwindsor/ase/lightpollution/repository/LightPollutionRepository.java
+++ src/main/java/com/uwindsor/ase/lightpollution/repository/LightPollutionRepository.java
@@ -1,9 +1,23 @@
 package com.uwindsor.ase.lightpollution.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.uwindsor.ase.lightpollution.model.PollutionReport;
+import com.uwindsor.ase.lightpollution.model.RecommendPlace;
 
 public interface LightPollutionRepository extends JpaRepository<PollutionReport, Long>{
+
+	@Query("select pr from PollutionReport pr where pr.image = (:image) AND pr.light_source = (:light_source) AND pr.brightness = (:brightness)")
+	PollutionReport findPollutionReport(@Param("image") String image, @Param("light_source") String light_source, @Param("brightness") String brightness);
+
+	@Query("select rp from RecommendPlace rp where rp.province = (:province)")
+	List<RecommendPlace> findPlacesByProvince(@Param("province") String province);
+
+	@Query("select rp from RecommendPlace rp where rp.city = (:city)")
+	List<RecommendPlace> findPlacesByCity(@Param("city") String city);
 
 }
