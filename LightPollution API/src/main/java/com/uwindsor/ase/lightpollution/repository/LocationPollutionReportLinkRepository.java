6ab3be681447ce161df37aa75a26bd350d917207
SakshiWadhwa <SakshiWadhwa@users.noreply.github.com>
2022-07-25 21:25:06 -0400

    Update LocationPollutionReportLinkRepository.java

--- LightPollution API/src/main/java/com/uwindsor/ase/lightpollution/repository/LocationPollutionReportLinkRepository.java
+++ LightPollution API/src/main/java/com/uwindsor/ase/lightpollution/repository/LocationPollutionReportLinkRepository.java
@@ -1,9 +1,12 @@
 package com.uwindsor.ase.lightpollution.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.uwindsor.ase.lightpollution.model.LocationMaster;
 import com.uwindsor.ase.lightpollution.model.LocationPollutionReportLink;
 import com.uwindsor.ase.lightpollution.model.PollutionReport;
 
@@ -11,7 +14,8 @@ public interface LocationPollutionReportLinkRepository extends JpaRepository<Loc
 
 	@Query("select lpr from LocationPollutionReportLink lpr where lpr.pollutionReport = (:pollutionReportCounter)")
 	LocationPollutionReportLink findByPollutionReportId(@Param("pollutionReportCounter") PollutionReport pollutionReportCounter);
-	
+
 	@Query("select lpr from LocationPollutionReportLink lpr where lpr.locationMaster = (:lm)")
 	List<LocationPollutionReportLink> fetchLocationPollutionLinkId(@Param("lm") LocationMaster lm);
 }
+
